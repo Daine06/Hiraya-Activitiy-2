e39486cf949f19dba2b83ee6bde1735768b5f633
Christian Dave Vila <cdavemvila@gmail.com>
2023-02-13 18:15:11 +0800

    Third push for the day

--- backend.py
+++ backend.py
@@ -1,18 +1,22 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask (__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+@app.route("/<usr>")
+def user(usr):
+    return render_template("user_profile.html") 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
