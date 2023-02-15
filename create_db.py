445761ccefbe5f24aa89c3b692a6b5cb402fc0a3
Christian Dave Vila <cdavemvila@gmail.com>
2023-02-15 10:22:02 +0800

    Second push (15/02/2023)

--- create_db.py
+++ create_db.py
@@ -1,15 +1,11 @@
 import sqlite3 as sql
 
-#connect to SQLite
 con = sql.connect('db_web.db')
 
-#Create a Connection
 cur = con.cursor()
 
-#Drop users table if already exsist.
 cur.execute("DROP TABLE IF EXISTS users")
 
-#Create users table  in db_web database
 sql ='''CREATE TABLE "users" (
 	"UID"	INTEGER PRIMARY KEY AUTOINCREMENT,
 	"UNAME"	TEXT,
@@ -18,8 +14,6 @@ sql ='''CREATE TABLE "users" (
 )'''
 cur.execute(sql)
 
-#commit changes
 con.commit()
 
-#close the connection
 con.close()
